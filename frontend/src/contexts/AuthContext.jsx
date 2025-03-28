5c6d31c690e2c938fddb24807790d4365aa38ef3
Sudesh <sudeshgosavi5002@gmail.com>
2025-03-28 11:36:56 -0500

    resolving issue

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import server from "../../enviroment";
+
 export const AuthContext = createContext({});
 
 const client = axios.create({
