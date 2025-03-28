859d7eefb3473d17a41b7b9302c053482d35c8dc
Sudesh <sudeshgosavi5002@gmail.com>
2025-03-28 11:56:38 -0500

    resolving issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -11,12 +10,12 @@ export default defineConfig({
     assetsDir: 'assets',
   },
 
-  
   server: {
-    host: '0.0.0.0', 
-    port: 8001      
+    host: '0.0.0.0',
+    port: 8001
   },
+
+  base: '/',  // Ensures correct asset loading
 
   plugins: [react()],
 })
-
